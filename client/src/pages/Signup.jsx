103c114e55e173f1623a79b8a4e2c179d01c14c7
Shubham1193 <shubhamsingh91930@gmail.com>
2024-05-31 08:46:42 +0530

    redux

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -47,9 +47,10 @@ const Signup = () => {
         {/* left */}
         <div className="flex-1">
           <Link to="/" className=" font-bold dark:text-white text-4xl">
-            <span className="px-2 py-1 bg-gradient-to-r from-indigo-600 via-purple-500 to-pink-500 rounded-lg text-white">
-              Blogger
+          <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+              Shubham's
             </span>
+            Blog
           </Link>
           <p className="text-sm mt-5">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
