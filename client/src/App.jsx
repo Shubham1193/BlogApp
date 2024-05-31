49cd5a542947d86ec88cd7888f5a2bf0f485bba4
Shubham1193 <shubhamsingh91930@gmail.com>
2024-05-31 10:45:07 +0530

    made dashborad private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Signup from "./pages/Signup";
 import Projects from "./pages/Projects";
 import Header from "./components/Header";
 import Footer from './components/Footer';
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
@@ -18,7 +19,9 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<Signin />} />
         <Route path="/sign-up" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route element={<PrivateRoute/>}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
         <Route path="/projects" element={<Projects />} />
       </Routes>
       <Footer />
